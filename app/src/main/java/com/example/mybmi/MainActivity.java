5eb27a50118663257572fbd6abcd1b8079780bbd
cflin <cflin@mail.cjcu.edu.tw>
2024-03-13 15:48:56 +0800

    計算BMI的功能   calcBMI

--- app/src/main/java/com/example/mybmi/MainActivity.java
+++ app/src/main/java/com/example/mybmi/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mybmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calcBMI(View view) {
+       EditText height = findViewById(R.id.etHeight);
+       EditText weight = findViewById(R.id.edWeight);
+       double h = Double.parseDouble(height.getText().toString());
+       double w = Double.parseDouble(weight.getText().toString());
+       double bmi = w / (h/100.0  * h /100.0);
+       String result = "你的BMI是 " + bmi;
+       TextView show =  findViewById(R.id.tvShow);
+       show.setText(result);
+
     }
 }
