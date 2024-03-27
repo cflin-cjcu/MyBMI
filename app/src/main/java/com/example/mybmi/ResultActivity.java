d5e181346ef3b18a41c9ada849edb4bc8f1806ff
cflin <cflin@mail.cjcu.edu.tw>
2024-03-27 15:43:50 +0800

    依照BMI顯示不同的圖片

--- app/src/main/java/com/example/mybmi/ResultActivity.java
+++ app/src/main/java/com/example/mybmi/ResultActivity.java
@@ -3,6 +3,7 @@ package com.example.mybmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
@@ -12,7 +13,17 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
         TextView show = findViewById(R.id.tvShowBMI);
+        ImageView img = findViewById(R.id.ivShow);
         double bmi = getIntent().getDoubleExtra("bmi", 0);
-        show.setText(String.valueOf(bmi));
+        if(bmi<18.5) {
+//            show.setText("體重過輕");
+            img.setImageResource(R.drawable.a3);
+        } else if (bmi<=24) {
+//            show.setText("體重正常");
+            img.setImageResource(R.drawable.a2);
+        } else {
+//            show.setText("體重過重");
+            img.setImageResource(R.drawable.a1);
+        }
     }
 }
