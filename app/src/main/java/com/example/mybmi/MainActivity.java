aa0e9fd5018ef9ad6b6dca6ee329dd85d974ea3a
cflin <cflin@mail.cjcu.edu.tw>
2024-04-03 15:12:35 +0800

    Radiobutton 的寫法

--- app/src/main/java/com/example/mybmi/MainActivity.java
+++ app/src/main/java/com/example/mybmi/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.mybmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
@@ -16,11 +19,28 @@ public class MainActivity extends AppCompatActivity {
     private EditText weight;
     private TextView show;
 
+    private RadioGroup rgSex;
+    private RadioButton rbMale;
+    private RadioButton rbFemale;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+
+        rgSex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if (checkedId == R.id.rbMale) {
+                    show.setText("我是男生");
+                } else if (checkedId == R.id.rbFemale) {
+                    show.setText("我是女生");
+                }
+            }
+        });
+
     }
 
     public void calcBMI(View view) {
@@ -46,6 +66,9 @@ public class MainActivity extends AppCompatActivity {
         height = findViewById(R.id.etHeight);
         weight = findViewById(R.id.edWeight);
         show = findViewById(R.id.tvShow);
+        rgSex = findViewById(R.id.rgSex);
+        rbMale = findViewById(R.id.rbMale);
+        rbFemale = findViewById(R.id.rbFemale);
     }
 
     public void GoNext(View view) {
