05e0fb326523cca46e7dd956044a534d783e84bc
cflin <cflin@mail.cjcu.edu.tw>
2024-04-24 15:24:01 +0800

    Listview 複選的練習

--- app/src/main/java/com/example/mybmi/ListActivity.java
+++ app/src/main/java/com/example/mybmi/ListActivity.java
@@ -21,27 +21,20 @@ public class ListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list);
 
         List<String> list = new ArrayList<>();
+
         list.add("蘋果");
         list.add("香蕉");
         list.add("橘子");
         list.add("西瓜");
-        list.add("西瓜");
-        list.add("西瓜");
-        list.add("西瓜");
-        list.add("西瓜");
-        list.add("西瓜");
-        list.add("西瓜");
-        list.add("西瓜");
-        list.add("西瓜");
-        list.add("西瓜");
+        list.add("葡萄");
+        list.add("芭樂");
 
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spfruit =  findViewById(R.id.spFruit);
         TextView tvshow1 = findViewById(R.id.tvShow1);
         ListView lvfruit = findViewById(R.id.lvFruit);
-
+        lvfruit.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 //        spfruit.setAdapter(adapter);
         lvfruit.setAdapter(adapter);
 
@@ -60,9 +53,17 @@ public class ListActivity extends AppCompatActivity {
         lvfruit.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                tvshow1.setText(list.get(position));
+                String s = "";
+                for (int i = 0; i < list.size(); i++) {
+                    if (lvfruit.isItemChecked(i)) {
+                        s += list.get(i) + " ";
+                    }
+                }
+                tvshow1.setText(s);
+
             }
         });
+
 
     }
 }
